80122786736f8ef9039198744fab3b8578310072
Leon <2446738735@qq.com>
2023-04-29 21:38:39 +0800

    4.29-20:43

--- src/views/PersonalView.vue
+++ src/views/PersonalView.vue
@@ -48,7 +48,7 @@
                     <a-input v-model:value="form.password" />
                 </a-form-item>
 
-                <a-form-item :wrapper-col="{ offset: 8, span: 16 }">
+                <a-form-item :wrapper-col="{ offset: 10, span: 16 }">
                     <a-button type="primary" html-type="submit">提交</a-button>
                 </a-form-item>
             </a-form>
